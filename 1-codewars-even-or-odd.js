ee245860c0c5c85ba72bc8bfb903e76a9380ff8a
hyeonshin <nashrullah1807@gmail.com>
2024-02-11 14:28:16 +0700

    Coding Challenge Hackerrank and Codewars Day (1/366)

--- 1-codewars-even-or-odd.js
+++ 1-codewars-even-or-odd.js
@@ -6,4 +6,4 @@
 //   return number % 2 == 0 ? "Even" : "Odd";
 // }
 
-const evenOrOdd = (number) => number % 2 == 0 ? "Even" : "Odd";
+const evenOrOdd = (number) => number % 2 === 0 ? "Even" : "Odd";
