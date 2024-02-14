bcf4ddc2d905d293dd09c0a68f6eb94a23bbe34c
hyeonshin <nashrullah1807@gmail.com>
2024-02-14 14:59:43 +0700

    Revert ".git"
    This reverts commit 8e2729dfd9a3ec91de0391b90f2b66ac62e38a4c.

--- 6-codewars-strings-end-with.js
+++
@@ -1,9 +0,0 @@
-//Coding challenge codewars day (6/366)
-//7 Kyu
-// https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/javascript
-
-//one liner first time
-// const solution = (str, ending) => str.endsWith(ending) ? true : false;
-
-//efficient
-const solution = (str, ending) => str.endsWith(ending);
