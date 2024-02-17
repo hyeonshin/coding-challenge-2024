420e3aa4cb2c0020e7908ff313b0748739778523
hyeonshin <nashrullah1807@gmail.com>
2024-02-17 11:02:53 +0700

    Coding Challenge Codewars (9/366)

---
+++ 9-codewars-no-space.js
@@ -0,0 +1,7 @@
+//Coding challenge codewars day (9/366)
+//8 Kyu
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
+
+//one liner first attempt
+const noSpace = x => x.split(" ").join("");
+
