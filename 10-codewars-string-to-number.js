a6f1560fa217b5edae8a283335e594cdd61bdc99
hyeonshin <nashrullah1807@gmail.com>
2024-02-18 21:02:58 +0700

    Coding Challenge Codewars (10/366)

---
+++ 10-codewars-string-to-number.js
@@ -0,0 +1,6 @@
+//Coding challenge codewars day (10/366)
+//8 Kyu
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
+
+//first attempt, one liner
+const stringToNumber = str => parseInt(str);
