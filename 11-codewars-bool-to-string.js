84f117df25278a000966c0d76fb484e7383b125d
hyeonshin <nashrullah1807@gmail.com>
2024-02-19 17:37:15 +0700

    Coding Challenge Codewars (11/366)

---
+++ 11-codewars-bool-to-string.js
@@ -0,0 +1,6 @@
+//Coding challenge codewars day (11/366)
+//8 Kyu
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+
+//one liner first attempt
+const booleanToString = b => b.toString();
