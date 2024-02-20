b04bc758f357685daacc9d37090d6c2cdc9256e3
hyeonshin <nashrullah1807@gmail.com>
2024-02-20 09:13:38 +0700

    Coding Challenge Codewars Day (12/366)

---
+++ 12-codewars-beginner-series-clock-2.js
@@ -0,0 +1,6 @@
+//Coding challenge codewars day (12/366)
+//8 Kyu
+// https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+
+//one liner, first attempt
+const past = (h,m,s) => (h*3600000) + (m*60000) + (s*1000);
