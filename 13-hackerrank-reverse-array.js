4364f869642736c06cb3badc87c97b0c0e961ffd
hyeonshin <nashrullah1807@gmail.com>
2024-02-21 09:06:46 +0700

    Coding Challenge Hackerrank Day (13/366)

---
+++ 13-hackerrank-reverse-array.js
@@ -0,0 +1,6 @@
+//Coding challenge hackerrank day (13/366)
+//Data Structure
+//https://www.hackerrank.com/challenges/arrays-ds/problem?isFullScreen=true
+
+//first attempt
+const reverseArray = a => a.reverse();
