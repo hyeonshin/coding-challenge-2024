262a37d8af1f0f3a6d75fbd65b30393e7dd01a5e
hyeonshin <nashrullah1807@gmail.com>
2024-02-21 08:57:18 +0700

    Coding Challenge Codewars Day (13/366)

---
+++ 13-codewars-reverse-string.js
@@ -0,0 +1,6 @@
+//Coding challenge codewars day (13/366)
+//8 Kyu
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+//one liner, first attempt
+const solution = str => str.split("").reverse().join("");
