635650c7af5dc8f9bd7421dc33ccb0d63ec5b9f1
hyeonshin <nashrullah1807@gmail.com>
2024-02-22 09:06:30 +0700

    Coding Challenge Codewars Day (14/366)

---
+++ 14-codewars-binary-addition.js
@@ -0,0 +1,6 @@
+//Coding challenge codewars day (14/366)
+//7 Kyu
+// https://www.codewars.com/kata/551f37452ff852b7bd000139/train/javascript
+
+//first attemp, one liner
+const addBinary = (a,b) => (a+b).toString(2);
