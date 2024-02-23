993eb205de748b82a3d080b6ec20b63c3564223d
hyeonshin <nashrullah1807@gmail.com>
2024-02-23 08:46:14 +0700

    Coding Challenge Codewars Day (15/366)

---
+++ 15-codewars-ones-and-zeros.js
@@ -0,0 +1,6 @@
+//Coding challenge codewars day (15/366)
+//7 Kyu
+// https://www.codewars.com/kata/578553c3a1b8d5c40300037c/train/javascript
+
+//first attempt, one liner
+const binaryArrayToNumber = arr => parseInt(arr.toString().replaceAll(',',''),2);
