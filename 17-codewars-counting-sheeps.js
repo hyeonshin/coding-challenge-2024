9a87f15f5e540c5d6a48484b9313e75991d1211e
hyeonshin <nashrullah1807@gmail.com>
2024-02-26 15:03:25 +0700

    Coding Challenge Codewars Day (17/366)

---
+++ 17-codewars-counting-sheeps.js
@@ -0,0 +1,22 @@
+//Coding challenge codewars day (16/366)
+//8 Kyu
+// https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+
+
+//first attempt
+function countSheeps(sheep) {
+    // TODO
+    let res = 0;
+    for (let i = 0; i < sheep.length; i++) {
+        console.log(sheep[i]);
+        if (sheep[i] == true && sheep[i] != undefined && sheep[i] != null) {
+            res++;
+        }
+    }
+    return res;
+}
+
+//efficient
+function countSheeps(arrayOfSheeps) {
+    return arrayOfSheeps.filter(Boolean).length;
+}
